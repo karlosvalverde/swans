7456f3b6b0096b66c555458ffe1485d2476a56f7
Carlos Valverde <studio@carlos-valverde.com>
2020-05-02 22:29:46 +0200

    [0.2.3][IMPROVED] Flexbox behaviour on small viewports

--- src/assets/routes/home.vue
+++ src/assets/routes/home.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="container-fluid">
+  <div class="container-fluid full">
   <div class="row">
-    <div class="col-6 home"></div>
-    <div class="col-6 align-self-center text-center">
-      <div class="border border-white m-5 p-5 text-white">
-        <h2>Hardest band on Earth</h2>
+    <div class="col home"></div>
+    <div class="break"></div>
+    <div class="col align-self-center text-center">
+      <div class="row">
+        <div class="col border border-white m-5 p-5 text-white">
+          <h2>Hardest band on Earth</h2>
+        </div>
+      </div>  
+      <div class="row">
+        <div class="col border border-white m-5 p-5 text-white overflow-auto">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
       </div>
     </div>
   </div>
